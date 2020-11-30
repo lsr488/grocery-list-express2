256d87701828380fcc32d5727420e7b31ffa36f1
unknown <lsr488@gmail.com>
2020-11-29 21:10:00 -0500

    BodyParser middleware added.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // static files
 app.use(express.static('public'));
